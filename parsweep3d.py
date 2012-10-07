532c025e231b538d24d09aee8e7cf6e3d120bbdc
marmaduke <duke@eml.cc>
2012-10-07 16:16:28 +0200

    step in right direction

--- parsweep3d.py
+++ parsweep3d.py
@@ -1,101 +1,94 @@
-import string, time, sys, data.dsi, multiprocessing
+import string, time, sys, data.dsi, multiprocessing, gc, util, itertools
 from pylab import *
 from numpy import *
-from cuda import *
 
-random.seed(42)
+model_nsvs = dict(fhn_euler=2, bistable_euler=1)
 
-def estnthr(dist, vel, dt, nsv, n, dispo=pycuda.autoinit.device.total_memory()):
-    idelmax = long(dist.max()/vel/dt)
-    # dispo >= 8*n*n + 4*nthr*(2 + n*nsv + n*idelmax)
-    return (dispo - 8*n*n)/(2 + n*nsv + n*idelmax)/4
+@util.subprocess
+def gpu(gsc, exc, vel, dt, dataset, nsv, tf=1500, ds=80, model="fhn_euler", cvar=0,
+        kblock=128, ublock=1024, cat=concatenate):
 
-# now goal is to obtain just cumsum(s**2/sum(s**2))[3]
-vel, gsc, exc = r_[1:11:10j], logspace(-4, 0, 256), r_[0.75:1.25:256j]
-G, E = meshgrid(gsc, exc)
-dataset = data.dsi.load_dataset('ay')
-n       = dataset.weights.shape[0]
-nic     = 5 
-nsv     = 2
-dt      = 0.1
-
-def gpu(G, E):
-    assert G.size % 256 == 0 
-    return randn(G.size)
-
-for i, v in enumerate(vel):
-
-    # determine launch config based on device characteristics
-    blockDim_x     = 256
-    gridDim_x      = 256
-    nthr           = estnthr(dataset.distances, v, dt, nsv, n)
-    if nthr/blockDim_x < grid:
-        gridDim_x = nthr/blockDim_x 
-    nthr = blockDim_x*gridDim_x
-
-    # implicit loop for n initial conditions per parameter set
-    G_, E_ = repeat(G.flat, nic)[:], repeat(E.flat, nic)[:]
-
-    nthrn = G_.size 
-    nlaunch = 1 + nthrn/nthr
-    print '%dth velocity %0.1f, %d launches, nthr %d' % (i, v, nlaunch, nthr)
-
-    Xss = []
-
-    if nthrn <= nthr:
-        Xss = [gpu(G_, E_)]
-    else:
-        for l in range(nlaunch-1):
-            Xss.append(gpu(G_[  l   *nthr :(l+1)*nthr ], E_[  l   *nthr :(l+1)*nthr ]))
-
-        if nthrn - (nic*G.size)/nthr*nthr > 0:
-            Xss.append(gpu(G_[ (l+1)*nthr :           ], E_[ (l+1)*nthr :           ]))
-            
-    
-    # reduce Xss correctly
-        
-        
-
-
-if 0:
-
-    tf      = 1500
-    ds      = 80
-    model   = "fhn_euler"
-    cvar    = 0
-
+    from cuda import srcmod, arrays_on_gpu # do not place in top of module!
 
     ts      = r_[0:tf:dt]
+    n       = dataset.weights.shape[0]
+    nthr    = len(gsc)
+    npad    = nthr % ublock if nthr%kblock or nthr%ublock else 0
+    nthr   += npad
+
     idel    = (dataset.distances/vel/dt).astype(int32)
     hist    = random.uniform(low=-1., high=1., size=(idel.max()+1, n, nthr))
     conn    = dataset.weights
     X       = random.uniform(low=-2, high=2, size=(n, nsv, nthr))
     Xs      = empty((1+len(ts)/ds, n, nsv, nthr), dtype=float32)
-    gsc     = -(logspace(-3.523, -0.745, npar)[:, newaxis])*ones((npar, nic)).astype(float32)
-    exc     = 1.01*ones((npar, nic)).astype(float32)
 
     hist[-1, ...] = X[:, cvar, :]
 
-    mem = sum(map(lambda a: a.nbytes, [idel, hist, conn, X, gsc, exc]))/2.0**20
-    print '%d iterations, %0.1f MB GPU, %0.1f addnl GPU' % (len(ts), mem, Xs.nbytes/2.**20)
+    mem = sum(map(lambda a: a.size*4, [idel, hist, conn, X, gsc, exc]))/2.0**20
+    print '%d iterations, %0.1f MB GPU, %0.1f addnl CPU' % (len(ts), mem, Xs.nbytes/2.**20)
 
-    mod = srcmod('parsweep.cu', ['kernel', 'update'],
+    mod = srcmod('parsweep.cu', ['kernel', 'update'], 
                  horizon=idel.max()+1, dt=dt, ds=ds, n=n, cvar=cvar, model=model, nsv=nsv)
 
     with arrays_on_gpu(_timed="integration", idel=idel.astype(int32), 
                        hist=hist.astype(float32), conn=conn.astype(float32), 
-                       X=X.astype(float32), exc=exc.astype(float32), gsc=gsc.astype(float32)) as g:
+                       X=X.astype(float32), exc=cat((exc, zeros((npad,)))).astype(float32), 
+                       gsc=cat((gsc, zeros((npad,)))).astype(float32)) as g:
 
         Xs[0, ...] = g.X.get()
         for step, t in enumerate(ts):
-            mod.kernel(int32(step), g.idel, g.hist, g.conn, g.X, g.gsc, g.exc, 
-                       block=(64, 1, 1), grid=(64, 1))
-            mod.update(int32(step), g.hist, g.X,
-                       block=(1024, 1, 1), grid=(4, 1))
+            mod.kernel(int32(step), g.idel, g.hist, g.conn, g.X, g.gsc, g.exc, block=(kblock, 1, 1), grid=(nthr/kblock, 1))
+            mod.update(int32(step), g.hist, g.X, block=(ublock, 1, 1), grid=(nthr/ublock, 1))
             if step%ds == 0 and not (1+step/ds)>=len(Xs):
-                Xs[1+step/ds, ...] = g.X.get()
+                Xs[1+step/ds, ...] = g.X.get().copy()
 
-    tic = time.time()
-    Xs -= Xs.mean(axis=0)
-    Xs = rollaxis(Xs, 3)
+    return rollaxis(Xs, 3)[:-npad]
 
+
+def launches(datasets, models, vel, G, E, nic, dt, blockDim_x=256, gridDim_x=256):
+
+    for d, m, v in itertools.product(datasets, models, vel):
+
+        nsv       = model_nsvs[model]
+        nthr      = util.estnthr(dataset.distances, v, dt, nsv)
+        gridDim_x = nthr/blockDim_x if nthr/blockDim_x < gridDim_x else gridDim_x
+        nthr      = blockDim_x*gridDim_x
+        G_, E_    = map(lambda a: repeat(a.flat, nic), meshgrid(gsc, exc))
+
+        if G_.size <= nthr:
+            yield G_, E_, v
+        else:
+            for l in range(G_.size/nthr-1):
+                yield G_[  l   *nthr :(l+1)*nthr ], E_[  l   *nthr :(l+1)*nthr ], v
+            if G_.size - G_.size/nthr*nthr > 0:
+                yield G_[ (l+1)*nthr :           ], E_[ (l+1)*nthr :           ], v
+
+
+def reducer(Xs):
+    Xs = Xs[:, -Xs.shape[1]/2:].reshape((G.size, -1, n*nsv)).copy() # copy to gc original
+
+    for j, X in enumerate(Xs):
+        try:
+            yield (lambda s: cumsum(s**2/sum(s**2))[3])(svd(X, full_matrices)[1])
+        except LinAlgError:
+            yield 1.0
+
+
+if __name__ == '__main__':
+
+    random.seed(42)
+
+    ngrid = 16
+    vel, gsc, exc = logspace(0, 2, 128), logspace(-4, -1.5, ngrid), r_[0.75:1.25:ngrid*1j]
+    datasets = [data.dsi.load_dataset('ay')]
+    models   = ['fhn_euler']
+    nic      = 32
+    nsv      = 2
+    dt       = 0.5
+    tf       = 500
+
+    for launch in launches(datasets, models, vel, gsc, exc, nic, dt):
+        temp = concatenate((temp.copy(), gpu(launch)))
+        if temp.shape[0] == G.size*nic:
+            for r in consumer(temp, nic):
+                print r
