72bd912072fad4716959b5e0b7f5163bc67e5cdf
marmaduke <duke@eml.cc>
2012-10-09 16:18:13 +0200

    Fixes a bug I had not encountered before and suppresses debug info while counting the launches.
    Worth noting that this is even possible in Python. I had
    used a keyword arg as a mutable local and written the code assuming that
    each loop inside the function saw that local with the default keyword
    value, but this is of course at best bad style.

--- parsweep3d.py
+++ parsweep3d.py
@@ -1,4 +1,6 @@
-import string, time, sys, data.dsi, multiprocessing, gc, util, itertools
+import string, time, sys, data.dsi, multiprocessing, gc, util, itertools, os
+from cuda import srcmod, arrays_on_gpu # do not place in top of module!
+import pycuda.driver
 from pylab import *
 from numpy import *
 
@@ -6,8 +8,6 @@ model_nsvs = dict(fhn_euler=2, bistable_euler=1)
 
 def gpu(gsc, exc, vel, dt, dataset, tf=1500, ds=80, model="fhn_euler", cvar=0,
         kblock=128, ublock=1024, cat=concatenate):
-    from cuda import srcmod, arrays_on_gpu # do not place in top of module!
-    import pycuda.driver
     ts      = r_[0:tf:dt]
     n       = dataset.weights.shape[0]
     nsv     = model_nsvs[model]
@@ -36,22 +36,27 @@ def gpu(gsc, exc, vel, dt, dataset, tf=1500, ds=80, model="fhn_euler", cvar=0,
                 Xs[1+step/ds, ...] = g.X.get()
     return (lambda X: X[:-npad] if npad else X)(rollaxis(Xs, 3))
 
-def launches(datasets, models, vel, gsc, exc, nic, dt, blockDim_x=256, gridDim_x=256):
+def launches(datasets, models, vel, gsc, exc, nic, dt, blockDim_x=256, gridDim_x_full=256, _just_counting=False):
     for i, cfg in enumerate(itertools.product(datasets, models, vel)):
         dataset, model, v = cfg
         nthr_     = util.estnthr(dataset.distances, v, dt, model_nsvs[model], dispo=5300*2**20)
-        gridDim_x = nthr_/blockDim_x if nthr_/blockDim_x < gridDim_x else gridDim_x
+        gridDim_x = nthr_/blockDim_x if nthr_/blockDim_x < gridDim_x_full else gridDim_x_full
         nthr      = blockDim_x*gridDim_x
         G_, E_    = map(lambda a: repeat(a.flat, nic), meshgrid(gsc, exc))
         nlaunch = G_.size/nthr + (0 if G_.size%nthr + nthr > nthr_ else -1)
-        print 'vel %0.2f, %d threads to run, blocks of %d possible, %d used in %d launches' % (v, G_.size, nthr_, nthr, nlaunch)
-        if G_.size <= nthr:
-            yield dataset, model, v, G_, E_
+
+        if _just_counting:
+            yield
         else:
-            for l in range(nlaunch):
-                yield dataset, model, v, G_[  l   *nthr :(l+1)*nthr ], E_[  l   *nthr :(l+1)*nthr ]
-            if G_.size - G_.size/nthr*nthr > 0:
-                yield dataset, model, v, G_[ (l+1)*nthr :           ], E_[ (l+1)*nthr :           ]
+            import pdb; pdb.set_trace()
+            print 'vel %0.2f, %d threads to run, blocks of %d possible, %d used in %d launches' % (v, G_.size, nthr_, nthr, nlaunch)
+            if G_.size <= nthr:
+                yield dataset, model, v, G_, E_
+            else:
+                for l in range(nlaunch):
+                    yield dataset, model, v, G_[  l   *nthr :(l+1)*nthr ], E_[  l   *nthr :(l+1)*nthr ]
+                if G_.size - G_.size/nthr*nthr > 0:
+                    yield dataset, model, v, G_[ (l+1)*nthr :           ], E_[ (l+1)*nthr :           ]
 
 def reducer(Xs, npar, nic):
     for X in Xs[:, -(Xs.shape[1]/2):].reshape((npar, Xs.shape[1]/2*nic, -1)).copy():
@@ -61,37 +66,42 @@ def reducer(Xs, npar, nic):
             yield 1.0
 
 if __name__ == '__main__':
+
     random.seed(42)
-    ngrid         = 32
-    vel, gsc, exc = logspace(-1, 2, ngrid), logspace(-5., -1.5, ngrid), r_[0.75:1.25:ngrid*1j]
+    ngrid         = 16
+    vel, gsc, exc = logspace(-1, 2, 32), logspace(-5., -1.5, ngrid), r_[0.95:1.1:ngrid*1j]
     datasets      = map(data.dsi.load_dataset, range(5))
-    models        = ['fhn_euler', 'bistable_euler']
-    nic           = 32
+    models        = ['fhn_euler']
+    nic           = 16
     dt            = 0.5
-    tf            = 1000
-    ds            = 50
-    results       = zeros(map(len, [datasets, models, vel, gsc, exc]))
-    buffer        = []
-    i             = 0
+    tf            = 50
+    ds            = 1
     j             = 0
     start_time    = time.time()
+    launch_count  = 0
 
-    print 'starting sweep of %d simulations' % (nic*ngrid**3,)
+    print 'counting launches needed...'
+    ls = launches(datasets, models, vel, gsc, exc, nic, dt, _just_counting=True)
+    for l in ls:
+        launch_count += 1
+    print 'launch count at ', launch_count
 
-    # producer, buffer, consumer (ish)
     for d, m, v, g, e in launches(datasets, models, vel, gsc, exc, nic, dt):
-        print j, 'th launch with', v, g.shape
-        #buffer.append(worker.apply(gpu, (g, e, v, dt, d), dict(tf=tf, model=m)))
-        result = gpu(g, e, v, dt, d, tf=tf, model=m, ds=ds)
-        print 'result.shape = ', result.shape
-        buffer.append(result)
-        j += 1
-        if sum([len(x) for x in buffer]) == len(gsc)*len(exc)*nic:
-            for r in reducer(concatenate(buffer), len(gsc)*len(exc), nic):
-                results.flat[i] = r
-                if i%(results.size/100) == 0:
-                    print '%2.f\t%d' % (time.time() - start_time, int(i/(results.size/100.0)))
-                i += 1
-            buffer = []
+        with open('./status.txt', 'w') as fd:
+            elapsed = time.time() - start_time
+            fd.write('%0.2f since start, %dth launch with vel %0.2f, nthr %r, %0.2f s remaining\n'
+                        % (elapsed, j, v, g.shape, elapsed/(j+1.) * (launch_count - j)))
+            fd.flush()
 
-    save('results.npy', results)
+        savename = 'launch-%06d.npy' % (j,)
+        try:
+            os.stat(savename)
+            print 'found this launch, skipping...'
+        except OSError:
+            try:
+                result = gpu(g, e, v, dt, d, tf=tf, model=m, ds=ds)
+                save(savename, result)
+                print 'result.shape = ', result.shape
+            except Exception as E:
+                print 'launch', j, v, 'borked with ', E
+        j += 1
