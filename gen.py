e07596eaf249a9cd0d27ab27150152c349b4638b
marmaduke <mmwoodman@gmail.com>
2012-10-23 12:56:55 +0200

    Cleanup

--- gen.py
+++ gen.py
@@ -1,35 +1,96 @@
+"""
+The gen module implements code generation for integrating a
+system's equations in different execution contexts and strategies.
+
+    - CPU
+    - CUDA
+
+can envision as well, where sensible, use of 
+
+    - CPU w/ OpenMP
+    - OpenCL
+
+Below, functions exist emitting various code:
+
+    - wrap - implements Python's % op in C
+    - step - advance integration one step
+    - model - differential equations for a particular node model
+
+Going forward, 
+
+    - Generalize coupling as f(x_post(t), x_pre(t-tau)), insert into
+        step's inner loop over delayed step
+    - Generalize integration scheme
+    - Add noise
+    - Add flexible monitor components
+    - Refactor into better pieces
+
+"""
+
 from cgen import *
 from cgen.cuda import *
 
 
 class RPointer(Pointer):
+    c99 = True
     def get_decl_pair(self):
         sub_tp, sub_decl = self.subdecl.get_decl_pair()
-        return sub_tp, ("* __restricted__ %s" % sub_decl)
+        restrict = " restrict " if self.c99 else " __restricted__ "
+        return sub_tp, "*%s%s" % (restrict, sub_decl)
 
-def wrap(self, horizon, gpu=False):
+def wrap(horizon, gpu=False, inline=True):
+    """
+    Emits a function that behaves like Python's modulo operator.
+    """
 
-    h = self.horizon
+    h = horizon
 
     fndecl = FunctionDeclaration(Value('int', 'wrap'), [Value('int', 'i')])
     if gpu:
         fndecl = CudaDevice(fndecl)
-    fndecl = DeclSpecifier(fndecl, 'inline')
+    if inline:
+        fndecl = DeclSpecifier(fndecl, 'inline')
 
     body = [If('i>=0', Block([Statement('return i %% %d' % h)]),
-                Block([If('i == - $horizon', 
+                Block([If('i == - %d' % h, 
                             Block([Statement('return 0')]),
-                            Block([Statement('%d + (i %% %d)' % (h, h))]))]))]
+                            Block([Statement('return %d + (i %% %d)' % (h, h))]))]))]
 
     return FunctionBody(fndecl, Block(body))
 
-def step(n, nsv, cvar=0, gpu=False, nunroll=1):
+def model(eqns, pars, name='model', dt=0.1, gpu=False, inline=True):
+
+    dtype = 'float' if gpu else 'double'
+
+    args = [RPointer(Value(dtype, 'X')), RPointer(Value('void', 'pars')), Value(dtype, 'input')]\
+         + ([Value('int', 'nthr'),Value('int', 'parij')] if gpu else [])
+
+    fndecl = FunctionDeclaration(Value('void', name), args)
+    if gpu:
+        fndecl = CudaDevice(fndecl)
+    if inline:
+        fndecl = DeclSpecifier(fndecl, 'inline')
+
+    Xrefs = [("X[ntr*%d + parij]" if gpu else "X[%d]") % i for i in range(len(eqns))]
+
+    body = [Initializer(Value(dtype, p), "((%s*) pars)[%d]" % (dtype, i)) for i, p in enumerate(pars)]\
+         + [Initializer(Value(dtype, eqn[0]), Xref) for Xref, eqn in zip(Xrefs, eqns)]\
+         + [Initializer(Value(dtype, 'd'+var), deriv) for var, deriv in eqns]\
+         + [Assign(Xref, '%s + %f*d%s' % (eqn[0], dt, var)) for Xref, eqn in zip(Xrefs, eqns)]
+
+    return FunctionBody(fndecl, Block(body))
+
+
+def step(n, nsv, cvar=0, gpu=False, nunroll=1, model='model'):
+    """
+    Emits a function that advances the system one step.
+    """
 
     dtype = 'float' if gpu else 'double'
 
     fndecl = FunctionDeclaration(Value('void', 'step'),
                [Value('int', 'step'), RPointer(Value('int', 'idel'))]\
-             + [RPointer(Value('float', arg)) for arg in ['hist', 'conn', 'X', 'gsc', 'exc']])
+             + [RPointer(Value(dtype, arg)) for arg in ['hist', 'conn', 'X', 'gsc', 'exc']])
 
     if gpu:
         fndecl = CudaGlobal(fndecl)
@@ -57,13 +118,13 @@ def step(n, nsv, cvar=0, gpu=False, nunroll=1):
 
     model_args = ['X + %s*i' % (('nthr*%d' if gpu else '%d')%nsv, ),
                   'exc' + (' + parij' if gpu else ''),
-                  'gsc%s*input/%d' % ('[parij]' if gpu else '', n)]\
+                  '%s*input/%d' % ('gsc[parij]' if gpu else '(*gsc)', n)]\
                + (['nthr', 'parij'] if gpu else [])
 
     body += [For('i=0', 'i<%d' % n, 'i++', Block([ 
         Assign('input', '0.0'),
         For('j=0', 'j<%d' % (n - n%nunroll,), '', Block(inner_loop_body*nunroll))
-    ] + inner_loop_body*(n%nunroll) + [Statement('model(%s)' % (', '.join(model_args), ))]
+    ] + inner_loop_body*(n%nunroll) + [Statement('%s(%s)' % (model, ', '.join(model_args), ))]
     )),
 
     # I thought this needed to be in a separate CUDA kernel to synch correctly, but I'm an idiot
@@ -73,36 +134,22 @@ def step(n, nsv, cvar=0, gpu=False, nunroll=1):
     return FunctionBody(fndecl, Block(body))
 
 
-def model(eqns, pars, dt=0.1, gpu=False):
+def module(model, step, wrap, gpu=False):
+    return Module([wrap, model, step])
 
-    dtype = 'float' if gpu else 'double'
-
-    args = [RPointer(Value(dtype, 'X')), RPointer(Value('void', 'pars')), Value(dtype, 'input')]\
-         + ([Value('int', 'nthr'),Value('int', 'parij')] if gpu else [])
-
-    fndecl = FunctionDeclaration(Value('void', 'fn'), args)
-    if gpu:
-        fndecl = CudaDevice(fndecl)
-    fndecl = DeclSpecifier(fndecl, 'inline')
-
-    Xrefs = [("X[ntr*%d + parij]" if gpu else "X[%d]") % i for i in range(len(eqns))]
-
-    body = [Initializer(Value(dtype, p), "((%s*) pars)[%d]" % (dtype, i)) for i, p in enumerate(pars)]\
-         + [Initializer(Value(dtype, eqn[0]), Xref) for Xref, eqn in zip(Xrefs, eqns)]\
-         + [Initializer(Value(dtype, 'd'+var), deriv) for var, deriv in eqns]\
-         + [Assign(Xref, '%s + %f*d%s' % (eqn[0], dt, var)) for Xref, eqn in zip(Xrefs, eqns)]
-
-    return FunctionBody(fndecl, Block(body))
-
+# put this into descr.py or somewhere else
 fhn = dict(
+    name='fhn',
     eqns=[('x', '(x - x*x*x/3.0 + y)*3.0/5.0'),
           ('y', '(a - x)/3.0/5.0 + input')],
     pars = ['a']
 )
 
 pitch = dict(
-    eqns=[('x', '(x - x*x*x/3.0)/5.0 + lambda')],
+    name='pitch',
+    eqns=[('x', '(x - x*x*x/3.0)/5.0 + lambda + input')],
     pars=['lambda']
 )
 
+    
 
